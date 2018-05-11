abf4e0213dc43eee9a88570c9b4f8ca0a193f27d
YINXIA LI <nmbmlyx0211@YINXIAs-Air.fios-router.home>
2018-05-11 11:17:27 -0400

    add java file

---
+++ SimpleCacheItem.java
@@ -0,0 +1,28 @@
+package assignments;
+
+
+public class SimpleCacheItem implements Cacheable<String> {
+
+    private final String name;
+    private int annualSalary;
+
+    public SimpleCacheItem(String name, int annualSalary) {
+        this.name = name;
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public String getKey() {
+        return name;
+    }
+
+
+    public int getAnnualSalary() {
+        return annualSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Salary for "+name+" : "+annualSalary;
+    }
+}
